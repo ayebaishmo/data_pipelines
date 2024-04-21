460761d475c4d79427eef7dfe9f55f8d9c1414f6
ayebaishmo <ishmo255super@gmail.com>
2024-04-21 13:20:13 +0300

    Added package file

--- databases/queries.py
+++ databases/queries.py
@@ -1,4 +1,4 @@
-GET_CHARACTERS = '''SELECT character_id, name FROM charactercreator_character;'''
+GET_CHARACTERS = '''SELECT * FROM charactercreator_character;'''
 
 
 CREATE_TEST_TABLE = '''
@@ -15,3 +15,12 @@ INSERT_TEST_TABLE = '''
     INSERT INTO test_table("name", "age", "country_of_origin")
     VALUES ('Rayn Allred', 30, 'Uganda');
 '''
+
+AVG_ITEM_WEIGHT_PER_CHARACTER = '''
+    SELECT cc_char.name, SUM(ai.weight)
+    FROM charactercreator_character as cc_char
+    JOIN charactercreator_character_inventory as CC_inv
+    ON cc_char.character_id = cc_inv.character_id
+    JOIN armory_item as ai
+    ON ai.item_id = cc_inv.item_id
+    GROUP BY cc_char.character_id'''
--- databases/sqlite_connection.py
+++ databases/sqlite_connection.py
@@ -1,19 +1,29 @@
 import sqlite3
-import queries as qu
+import queries as q
 import pandas as pd
-def connect_to_db(db_name = 'rpg_db.sqlite3'):
+
+# DB Connect function
+def connect_to_db(db_name='rpg_db.sqlite3'):
     return sqlite3.connect(db_name)
 
 def execute_q(conn, query):
+    # step 2- Make the cursor
     curs = conn.cursor()
+    # Execute the query
     curs.execute(query)
+    # pull and return
     return curs.fetchall()
 
-if __name__ == '__main__':
+
+if __name__=="__main__":
     conn = connect_to_db()
-    results = execute_q(conn, qu.GET_CHARACTERS)
-    # print(results[::5])
+    # print(execute_q(conn, q.AVG_ITEM_WEIGHT_PER_CHARACTER)[:5])
+    results = execute_q(conn, q.AVG_ITEM_WEIGHT_PER_CHARACTER)
 
     # change the tuples to dataframes
     df = pd.DataFrame(results)
+    # Lets name the columns
+    df.columns = ['name', 'average_item_weight']
+    # turn the dataframe and save it to csv file
+    df.to_csv('rpg_db.csv', index=False)
     print(df.head())
